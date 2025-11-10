644e69ff4e76f74403918bf46f4665ce7cbf5a12
nhathuanxx <nhathuanxx@gmail.com>
2025-11-10 09:28:23 +0700

    sửa wp-config

--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,6 @@
  *
  * @package WordPress
  */
-
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 // define( 'DB_NAME', 'wp_vinamek' );
@@ -90,7 +89,9 @@ $table_prefix = 'wp_';
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define( 'WP_DEBUG', false );
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', true);
 
 /* Add any custom values between this line and the "stop editing" line. */
 
