2f71183fc77e626fa2a1eedcc251d97698b17310
nhathuanxx <nhathuanxx@gmail.com>
2025-11-10 09:51:24 +0700

    sửa wp-config

--- wp-config.php
+++ wp-config.php
@@ -89,9 +89,10 @@ $table_prefix = 'wp_';
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define('WP_DEBUG', false);
-// define('WP_DEBUG_LOG', true);
-// define('WP_DEBUG_DISPLAY', true);
+define('WP_DEBUG', true);        // vẫn ghi log
+define('WP_DEBUG_LOG', true);    // ghi vào wp-content/debug.log
+define('WP_DEBUG_DISPLAY', false); // KHÔNG hiển thị lỗi trên trang
+@ini_set('display_errors', 0);
 
 /* Add any custom values between this line and the "stop editing" line. */
 
